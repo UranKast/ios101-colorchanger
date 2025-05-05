039ccd5363a981772576d7d530d0d524eb389c36
Uran <kastratiuran@gmail.com>
2025-05-04 23:33:16 -0400

    Initial working version: random background color app

--- ColorChanger/ViewController.swift
+++ ColorChanger/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        let newColor = randomColor()
+        self.view.backgroundColor = newColor
     }
 
-
+    private func randomColor() -> UIColor {
+        let red   = CGFloat.random(in: 0...1)
+        let green = CGFloat.random(in: 0...1)
+        let blue  = CGFloat.random(in: 0...1)
+        return UIColor(red: red, green: green, blue: blue, alpha: 1.0)
+    }
 }
 
